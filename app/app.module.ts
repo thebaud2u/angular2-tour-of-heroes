298de4fe10d5e8fb943d046abdc31931eb5a46b7
Thébaud Jean-Clément <thebaud.clement@gmail.com>
2016-09-22 17:22:22 +0200

    Now every component has it's own html/css/ts file, everything is seperated

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,18 +2,28 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
-import { AppComponent }  from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import { AppComponent }  		from './app.component';
+import { DashboardComponent }	from './dashboard.component';
+import { HeroDetailComponent } 	from './hero-detail.component';
+import { HeroesComponent }  	from './heroes.component';
+import { HeroService }  		from './hero.service';
+import { routing }  			from './app.routing';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  declarations: [
-    AppComponent,
-    HeroDetailComponent
-  ],
-  bootstrap: [ AppComponent ]
+  	imports: [
+    	BrowserModule,
+    	FormsModule,
+    	routing
+  	],
+  	declarations: [
+    	AppComponent,
+    	DashboardComponent,
+    	HeroDetailComponent,
+    	HeroesComponent
+  	],
+  	providers: [
+  		HeroService
+  	],
+  	bootstrap: [ AppComponent ]
 })
 export class AppModule { }
